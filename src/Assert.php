5de4ab4a8f230053d374d390435550a0fc765c2f
zander <13566040784@163.com>
2018-08-20 16:34:48 +0800

    aa

---
+++ src/Assert.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the webmozart/assert package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace zander84\modernadmin;
+
+use ArrayAccess;
+use BadMethodCallException;
+use Closure;
+use Countable;
+use Exception;
+use InvalidArgumentException;
+use Throwable;
+use Traversable;
+
+class Assert
+{
+   public static function a()
+   {
+       echo 124;
+   }
+}
