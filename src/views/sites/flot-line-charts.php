870179ba53e563c6ba30c229e879cef112ae372a
zander <13566040784@163.com>
2018-11-30 23:05:28 +0800

    dashboard

--- src/views/sites/flot-line-charts.php
+++ src/views/sites/flot-line-charts.php
@@ -3,186 +3,192 @@
 $this->title = 'Modern';
 $baseUrl = \zander84\modernadmin\widgets\ModernRegisterAsset::widget([
     'assets' => [
-            \zander84\modernadmin\assets\FlotLineChartsAsset::class,
+        \zander84\modernadmin\assets\FlotLineChartsAsset::class,
     ]
 ]);
 ?>
 
 <div class="content-header row">
-<div class="content-header-left col-md-6 col-12 mb-2 breadcrumb-new">
-<h3 class="content-header-title mb-0 d-inline-block">Flot Line Chart</h3>
-<div class="row breadcrumbs-top d-inline-block">
-<div class="breadcrumb-wrapper col-12">
-<ol class="breadcrumb">
-<li class="breadcrumb-item"><a href="index.html">Home</a>
-</li>
-<li class="breadcrumb-item"><a href="flot-line-charts.html#">Flot Charts</a>
-</li>
-<li class="breadcrumb-item active">Flot Line Chart
-                </li>
-</ol>
-</div>
-</div>
-</div>
-<div class="content-header-right col-md-6 col-12">
-<div class="dropdown float-md-right">
-<button aria-expanded="false" aria-haspopup="true" class="btn btn-danger dropdown-toggle round btn-glow px-2" data-toggle="dropdown" id="dropdownBreadcrumbButton" type="button">Actions</button>
-<div aria-labelledby="dropdownBreadcrumbButton" class="dropdown-menu"><a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-calendar-check-o"></i> Calender</a>
-<a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-cart-plus"></i> Cart</a>
-<a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-life-ring"></i> Support</a>
-<div class="dropdown-divider"></div><a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-cog"></i> Settings</a>
-</div>
-</div>
-</div>
+    <div class="content-header-left col-md-6 col-12 mb-2 breadcrumb-new">
+        <h3 class="content-header-title mb-0 d-inline-block">Flot Line Chart</h3>
+        <div class="row breadcrumbs-top d-inline-block">
+            <div class="breadcrumb-wrapper col-12">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="flot-line-charts.html#">Flot Charts</a>
+                    </li>
+                    <li class="breadcrumb-item active">Flot Line Chart
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <div class="content-header-right col-md-6 col-12">
+        <div class="dropdown float-md-right">
+            <button aria-expanded="false" aria-haspopup="true"
+                    class="btn btn-danger dropdown-toggle round btn-glow px-2" data-toggle="dropdown"
+                    id="dropdownBreadcrumbButton" type="button">Actions
+            </button>
+            <div aria-labelledby="dropdownBreadcrumbButton" class="dropdown-menu"><a class="dropdown-item"
+                                                                                     href="flot-line-charts.html#"><i
+                            class="la la-calendar-check-o"></i> Calender</a>
+                <a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-cart-plus"></i> Cart</a>
+                <a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-life-ring"></i> Support</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="flot-line-charts.html#"><i class="la la-cog"></i> Settings</a>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="content-body">
-<!-- Flot line charts section start -->
-<section id="flot-line-charts">
-<!-- Line Chart -->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h4 class="card-title">Line Chart</h4>
-<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-<div class="heading-elements">
-<ul class="list-inline mb-0">
-<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-<li><a data-action="close"><i class="ft-x"></i></a></li>
-</ul>
-</div>
-</div>
-<div class="card-content collapse show">
-<div class="card-body">
-<div class="height-400" id="line-chart"></div>
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- Tracking Chart -->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h4 class="card-title">Tracking Chart</h4>
-<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-<div class="heading-elements">
-<ul class="list-inline mb-0">
-<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-<li><a data-action="close"><i class="ft-x"></i></a></li>
-</ul>
-</div>
-</div>
-<div class="card-content collapse show">
-<div class="card-body">
-<div class="height-400" id="tracking"></div>
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- Realtime Updates -->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h4 class="card-title">Realtime Updates</h4>
-<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-<div class="heading-elements">
-<ul class="list-inline mb-0">
-<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-<li><a data-action="close"><i class="ft-x"></i></a></li>
-</ul>
-</div>
-</div>
-<div class="card-content collapse show">
-<div class="card-body">
-<div class="height-400" id="realtime"></div>
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- Series Types -->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h4 class="card-title">Series Types</h4>
-<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-<div class="heading-elements">
-<ul class="list-inline mb-0">
-<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-<li><a data-action="close"><i class="ft-x"></i></a></li>
-</ul>
-</div>
-</div>
-<div class="card-content collapse show">
-<div class="card-body">
-<div class="height-400" id="series-types"></div>
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- Symbols -->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h4 class="card-title">Symbols</h4>
-<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-<div class="heading-elements">
-<ul class="list-inline mb-0">
-<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-<li><a data-action="close"><i class="ft-x"></i></a></li>
-</ul>
-</div>
-</div>
-<div class="card-content collapse show">
-<div class="card-body">
-<div class="height-400" id="symbols"></div>
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- Visitors -->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h4 class="card-title">Visitors</h4>
-<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-<div class="heading-elements">
-<ul class="list-inline mb-0">
-<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-<li><a data-action="close"><i class="ft-x"></i></a></li>
-</ul>
-</div>
-</div>
-<div class="card-content collapse show">
-<div class="card-body">
-<div class="height-400" id="visitors-placeholder"></div>
-<div id="visitors-overview" style="height:100px;"></div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
-<!-- // Flot line charts section end -->
+    <!-- Flot line charts section start -->
+    <section id="flot-line-charts">
+        <!-- Line Chart -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Line Chart</h4>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                <li><a data-action="close"><i class="ft-x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body">
+                            <div class="height-400" id="line-chart"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Tracking Chart -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Tracking Chart</h4>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                <li><a data-action="close"><i class="ft-x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body">
+                            <div class="height-400" id="tracking"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Realtime Updates -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Realtime Updates</h4>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                <li><a data-action="close"><i class="ft-x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body">
+                            <div class="height-400" id="realtime"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Series Types -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Series Types</h4>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                <li><a data-action="close"><i class="ft-x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body">
+                            <div class="height-400" id="series-types"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Symbols -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Symbols</h4>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                <li><a data-action="close"><i class="ft-x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body">
+                            <div class="height-400" id="symbols"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Visitors -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Visitors</h4>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                <li><a data-action="close"><i class="ft-x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body">
+                            <div class="height-400" id="visitors-placeholder"></div>
+                            <div id="visitors-overview" style="height:100px;"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- // Flot line charts section end -->
 </div>
