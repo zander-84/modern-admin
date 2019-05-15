7c039d73417cadd336ac6a9b8b6b422426600d1b
zander <13566040784@163.com>
2019-05-15 16:05:42 +0800

    wechat miniprogram

--- src/helpers/wechat/lib/WxPayNotify.php
+++ src/helpers/wechat/lib/WxPayNotify.php
@@ -9,11 +9,33 @@
 namespace zander84\modernadmin\helpers\wechat\lib;
 
 use zander84\modernadmin\helpers\wechat\lib\paydata\WxPayNotifyReply;
+use zander84\modernadmin\helpers\wechat\lib\paydata\WxPayOrderQuery;
 
 class WxPayNotify extends WxPayNotifyReply
 {
 	private $config = null;
 	public $callback ;
+
+
+    //查询订单
+    public function Queryorder($transaction_id, $config)
+    {
+        $input = new WxPayOrderQuery();
+        $input->SetTransaction_id($transaction_id);
+
+        $result = WxPayApi::orderQuery($config, $input);
+
+        if(array_key_exists("return_code", $result)
+            && array_key_exists("result_code", $result)
+            && $result["return_code"] == "SUCCESS"
+            && $result["result_code"] == "SUCCESS")
+        {
+            return true;
+        }
+        return false;
+    }
+
+
 	/**
 	 * 
 	 * 回调入口
@@ -56,10 +78,43 @@ class WxPayNotify extends WxPayNotifyReply
 	 */
 	public function NotifyProcess($objData, $config, &$msg)
 	{
-        file_put_contents(\Yii::$app->getRuntimePath() . '/logs/wechatminiprogram.log','aaa',FILE_APPEND);
-		//TODO 用户基础该类之后需要重写该方法，成功的时候返回true，失败返回false
+
+        $data = $objData->GetValues();
+        //TODO 1、进行参数校验
+        if(!array_key_exists("return_code", $data)
+            ||(array_key_exists("return_code", $data) && $data['return_code'] != "SUCCESS")) {
+            //TODO失败,不是支付成功的通知
+            //如果有需要可以做失败时候的一些清理处理，并且做一些监控
+            $msg = "异常异常";
+            return false;
+        }
+        if(!array_key_exists("transaction_id", $data)){
+            $msg = "输入参数不正确";
+            return false;
+        }
+
+        //TODO 2、进行签名验证
+        try {
+            $checkResult = $objData->CheckSign($config);
+            if($checkResult == false){
+                //签名错误
+                $msg = "签名错误";
+                return false;
+            }
+        } catch(Exception $e) {
+            $msg = "签名错误";
+            return false;
+        }
+
+        //TODO 3、查询订单，判断订单真实性
+        if(!$this->Queryorder($data["transaction_id"], $config)){
+            $msg = "订单查询失败";
+            return false;
+        }
+
+		//TODO 4、业务处理
         $c = $this->callback;
-        return $c($objData, $config, $msg);
+        return $c($data, $msg);
 		//return false;
 	}
 
